bdf82f11f8d1a3d4dea5a79daaa38362a23697c9
rahul_reddy <rahul_reddy@symantec.com>
2018-02-17 00:00:35 -0800

    Initial Commit

---
+++ reboot.sh
@@ -0,0 +1,3 @@
+LIST_OF_HOST=$1
+for i in `cat $LIST_OF_HOST`; do aws ec2 describe-instances --filters "Name=private-ip-address,Values=$i"  --query 'Reservations[*].Instances[*].[InstanceId]' --profile default --region us-east-1 | tr '"' '\n' | grep ^i- | awk '{print "aws ec2 reboot-instances --instance-ids "  $1 " --profile default --region us-east-1"}' | sh; sleep 300; done
+
